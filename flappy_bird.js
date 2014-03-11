329fea0af5b94a52b3dd1b6283a100edd92956cd
Ben Sklaroff <bsklaroff@gmail.com>
2014-03-11 03:52:29 -0700

    make basic bird y-movement

--- flappy_bird.js
+++ flappy_bird.js
@@ -8,11 +8,20 @@ var context = canvas.getContext('2d');
 //---------- GAME GLOBAL VARIABLES ----------//
 //-------------------------------------------//
 // Game dimensions
-var width = 144;
-var height = 257;
+var scale = 3;
+var width = 144 * scale;
+var height = 257 * scale;
 // Set canvas dimensions to correct width and height
 canvas.width = width;
 canvas.height = height;
+var gameState = 'ready';
+var bird = {
+  w: 17 * scale,
+  h: 12 * scale,
+  x: 35 * scale,
+  y: 120 * scale,
+  v: 0
+};
 //
 // ADD MORE VARIABLES HERE!!!
 //
@@ -21,15 +30,18 @@ canvas.height = height;
 //---------------------------------------//
 //---------- PRELOADING IMAGES ----------//
 //---------------------------------------//
-// Define images
-var bgImg = new Image();
-bgImg.src = "imgs/background.png";
-//
-// ADD MORE IMAGES HERE!!!
-//
-
+// Create mapping from image names to objects
+var imgs = {};
+var addImg = function(name, src) {
+  imgs[name] = new Image();
+  imgs[name].src = src;
+  imgs[name].onload = function() {
+    imageLoaded();
+  }
+  numImages++;
+};
 // Ensure all images have loaded before starting the game
-var numImages = 1; // CHANGE THIS NUMBER ONCE YOU ADD MORE IMAGES!!!
+var numImages = 0;
 var numLoaded = 0;
 var imageLoaded = function() {
   numLoaded++;
@@ -37,13 +49,12 @@ var imageLoaded = function() {
     initGame();
   }
 };
-bgImg.onload = function() {
-  imageLoaded();
-};
+// Define images
+addImg('bg', 'imgs/background.png');
+addImg('bird_up', 'imgs/bird_up.png');
 //
-// ADD MORE ONLOAD CALLBACKS HERE!!!
+// ADD MORE IMAGES HERE!!!
 //
-
 
 
 //-----------------------------------------//
@@ -67,6 +78,14 @@ var onCanvasMouseDown = function(e) {
   // Find the mouse x and y relative to the top-left corner of the canvas
   var x = e.layerX;
   var y = e.layerY;
+  if (gameState === 'ready') {
+    gameState = 'play';
+  }
+  if (gameState === 'play') {
+    bird.v = Math.min(0, bird.v);
+    bird.v -= 3 * scale;
+    bird.v = Math.max(-3 * scale, bird.v);
+  }
   //
   // ADD CLICK HANDLING CODE HERE!!!
   //
@@ -90,12 +109,18 @@ window.addEventListener('keydown', onCanvasKeyDown);
 //------------------------------------//
 var updateGame = function() {
   // Draw the background image
-  context.drawImage(bgImg, 0, 0, width, height);
+  context.drawImage(imgs.bg, 0, 0, width, height);
 
+  if (gameState === 'play') {
+    bird.y += bird.v;
+    bird.v += .2 * scale;
+  }
   //
   // ADD GAME LOGIC HERE!!!
   //
 
+  // Draw the bird
+  context.drawImage(imgs.bird_up, bird.x, bird.y, bird.w, bird.h);
   //
   // ADD MORE IMAGE DRAWING HERE!!!
   //
